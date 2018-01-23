2854912969a7c589ac1ca55713b68629909b927c
lewisHoulihan <G00344955@gmit.ie>
2018-01-23 17:30:43 +0000

    Functions.ts added to files + typesScript

---
+++ typesScript.js
@@ -0,0 +1,18 @@
+//boolean
+var isDone = false;
+//numbers
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 3872;
+//Strings
+var color = "blue";
+color = 'red';
+//Array
+var list = [1, 2, 3];
+console.log("Boolean: " + isDone);
+console.log("Decimal: " + decimal);
+console.log("Binary: " + hex);
+console.log("Octal: " + octal);
+console.log("String: " + color);
+console.log("Array" + list);
---
+++ typesScript.ts
@@ -0,0 +1,19 @@
+//boolean
+let isDone: boolean = false;
+//numbers
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o7440;
+//Strings
+let color: string = "blue";
+color = 'red';
+//Array
+let list: number[] = [1, 2, 3];
+
+console.log("Boolean: " + isDone);
+console.log("Decimal: " + decimal);
+console.log("Binary: " + hex);
+console.log("Octal: " + octal);
+console.log("String: " + color);
+console.log("Array"+list);
